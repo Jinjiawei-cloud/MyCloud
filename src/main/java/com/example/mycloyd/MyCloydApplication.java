2c184e859f24b15590cd0af8cf490a52935a8dfe
gregoryjin <gregory4273@163.com>
2023-02-13 01:20:41 +0800

    Initial commit

---
+++ src/main/java/com/example/mycloyd/MyCloydApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycloyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCloydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCloydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mycloyd/MyCloydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycloyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCloydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
